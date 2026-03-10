c45675c861779c5cd9f79a45bf4d4750d62a99da
jotave <joao.v.souza193@aluno.senai.br>
2026-03-10 14:26:02 -0300

    Add files via upload

--- data/database.js
+++ data/database.js
@@ -1,29 +1,40 @@
 let categorias = [
-    {id: 1, nome: "Churrasco"},
-    {id: 2, nome: "Entradinhas"},
-    {id: 3, nome: "Bebidas"},
+    {
+        id: 1,
+        nome: "Carnes"
+    },
+    {
+        id: 2,
+        nome: "Acompanhamentos"
+    },
+    {
+        id: 3,
+        nome: "Bebidas"
+    }
 ];
 
 let produtos = [
-    { categoriaId: 1,
-    nome: "Churrasco de Picanha",
-    preco: 59.90,
-    descrição:"Delicioso churrasco de picanha, acompanhado de farofa e vinagrete",
-    imagem:"https://i.ytimg.com/vi/KgoqcbuZEBg/maxresdefault.jpg"},
-
-    { categoriaId: 2,
-    nome: "Torresmo Crocante",
-    preco: 30.00,
-    descrição:"Torresmo crocante, servido com molho de pimenta e limão",
-    imagem:"https://midias.agazeta.com.br/2024/04/30/festival-torresmofest-no-boulevard-shopping-em-vila-velha---2089478-article.jpg"},
-
-    { categoriaId: 3,
-    nome: "Refrigerante 350ml",
-    preco: 7.50,
-    descrição:"Refrigerante em lata de 350ml, escolha entre Coca-Cola, Guaraná ou Sprite",
-    imagem:"https://horasagradamc.meucatalogofacil.com/_core/_uploads/14/2020/11/111328112016gk25fh0h.jpeg"},s
-
-
+    { 
+        id: 1,
+        nome: "Picanha Fatiada na Brasa (Para 2 Pessoas)",
+        preco: 149.90,
+        descrição: "Suculenta picanha fatiada servida na chapa quente. Acompanha arroz branco, farofa da casa, vinagrete e batata frita.",
+        imagem: "https://minhasreceitinhas.com.br/wp-content/uploads/2023/08/picanha-grelhada-e1691083250502.jpg"
+    },
+    { 
+        id: 2,
+        nome: "Porção de Pão de Alho Tradicional",
+        preco: 22.50,
+        descrição: "5 unidades do nosso famoso pão de alho cremoso, assado na brasa até ficar dourado e crocante por fora.",
+        imagem: "https://www.sabornamesa.com.br/media/k2/items/cache/68fd1d661976b9d8b879f7809970f2e8_XL.jpg"
+    },
+    { 
+        id: 3,
+        nome: "Chopp Pilsen 500ml",
+        preco: 14.00,
+        descrição: "Chopp claro estupidamente gelado, tirado na hora com o colarinho perfeito para acompanhar o seu churrasco.",
+        imagem: "https://d21wiczbqxib04.cloudfront.net/yY566PdzrtwNzsQ2bmDOjnXLZ4E=/1600x0/smart/https://osuper-ecommerce-correa.s3.sa-east-1.amazonaws.com/d7149853-whatsapp-image-20211008-at-121958.jpeg"
+    }
 ];
 
 module.exports = { categorias, produtos };
